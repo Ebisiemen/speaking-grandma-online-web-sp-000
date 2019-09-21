83b956ff78d005caf715a7d000203288db71e941
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-21 06:48:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,9 @@
 def speak_to_grandma(phrase = "")
   if "#{phrase}" == "#{phrase}".downcase 
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif "#{phrase}" == "#{phrase}".upcase 
-  puts "NO, NOT SINCE 1938!"
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif "#{phrase}" == "#{phrase}".upcase 
+    puts "NO, NOT SINCE 1938!"
+  else  "#{phrase}" == "I LOVE YOU TOO PUMPKIN!" 
 end   
 
 
